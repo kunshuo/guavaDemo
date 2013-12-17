4eca943115e3b0704f948e58a652b2b2e40b102b
kunshuo <swujianchen@163.com>
2013-12-17 18:12:30 +0800

    增加测试方法

--- src/test/java/com/jianchen/guava/base/TestBase.java
+++ src/test/java/com/jianchen/guava/base/TestBase.java
@@ -1,19 +1,40 @@
 package com.jianchen.guava.base;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
 import com.jianchen.guava.po.Person;
 import junit.framework.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * User: kunshuo
  * Date: 13-12-17 Time: ÏÂÎç3:57
  * company:jianchen.com
  */
+@RunWith(JUnit4.class)
 public class TestBase {
     @Test
-    public  void testToString(){
-        Person person = new Person("jianchen",25,"MALE");
+    public void testToString() {
+        Person person = new Person("jianchen", 25, "MALE");
         Assert.assertNotNull(person.toString());
-        Assert.assertEquals("Person{name=jianchen, age=25, sex=MALE}",person.toString());
+        Assert.assertEquals("Person{name=jianchen, age=25, sex=MALE}", person.toString());
     }
+
+    @Test
+    public void testOptional() {
+        Optional<Integer> optional = Optional.of(5);
+        Assert.assertNotNull(optional.get());
+    }
+
+    @Test
+    public void testSplitter(){
+        String str1 = "my,name:is;jianchen";
+        System.out.println(Splitter.on(CharMatcher.anyOf(":,;")).split(str1));  //[my, name, is, jianchen]
+
+    }
+
 }
